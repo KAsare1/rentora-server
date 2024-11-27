ab768de4c95b3e177f1735ceb2d64291e13cc26d
MarkKMensah <mamensah@ug.edu.gh>
2024-11-27 18:07:31 +0000

    db updates from office

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,9 +5,12 @@ import (
 	"net/http"
 	"os"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
+	"github.com/joho/godotenv"
+
+	
 	"rentora-go/internal/handler"
 	"rentora-go/internal/model"
 	"rentora-go/internal/repository"
@@ -15,8 +18,19 @@ import (
 )
 
 func main() {
+	// Get database credentials from environment variables
+	err := godotenv.Load()
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Construct DSN (Data Source Name)
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
 	// Initialize the database
-	db, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
